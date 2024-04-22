d7fee67135152638a4c312f16771022772b5eee5
kacper-wlodarek <166040074+kacper-wlodarek@users.noreply.github.com>
2024-04-22 09:18:53 +0000

    Update backend.tf

--- kacper-playground-master/backend.tf
+++ kacper-playground-master/backend.tf
@@ -7,7 +7,7 @@ terraform {
     use_azuread_auth     = true
     # Using Managed Identities is not supported from anything other than a VM / VM Scaleset at the moment.
     # https://github.com/hashicorp/terraform-provider-azurerm/issues/5558
-    subscription_id = "b62bcb25-4b55-4b54-8699-7aab9698a969"
-    tenant_id       = "d93d9ab1-a1ed-4bde-9725-0cdfdea791d2"
+    subscription_id = "xyqzcqwe-4b55-4b54-8699-7aab9698a969"
+    tenant_id       = "xxxxxxxx-yyyy-yyyy-9725-0cdfdea791d2"
   }
 }
