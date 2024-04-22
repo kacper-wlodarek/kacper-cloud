2473dc2374e3fb3bf801722a98f9b9fa0e25fb7e
kacper-wlodarek <166040074+kacper-wlodarek@users.noreply.github.com>
2024-04-22 09:17:06 +0000

    Update variables.tf

--- kacper-playground-master/variables.tf
+++ kacper-playground-master/variables.tf
@@ -1,5 +1,5 @@
 variable "subscription_id" {
   type        = string
   description = "The subscription to deploy the resources to."
-  default = "2c735461-ecab-4a9a-afc2-620d7eed86e2"
+  default = "2c735461-xxxx-xxxx-yyyy-620d7eed86e2"
 }
